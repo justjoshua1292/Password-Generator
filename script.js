a5b5ac9ee90a9cee2801a0fe5167fb416158ce73
Joshua Harris <Justjoshua1292@gmail.com>
2021-06-21 12:53:32 -0400

    done

--- script.js
+++ script.js
@@ -91,11 +91,9 @@ function generatePassword() {
 
     if(confirmNumber) {
         // add numbers!
-        // [] + ["!", "@", "#", "$", "%", "^","&", "*",...] = [a, b, c, d, e....]
+        // [] + [0, 1, 2, 3, 4, 5, 6, 7, 8, 9...] = [a, b, c, d, e....]
         userChoices = userChoices.concat(Number);
     }
-
-
 
     console.log(userChoices)
 
