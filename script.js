594005501ad3b1cd24b7ba5adeef8385400605d9
Joshua Harris <Justjoshua1292@gmail.com>
2021-06-21 12:34:32 -0400

    added password requirements and various choices

--- script.js
+++ script.js
@@ -5,22 +5,26 @@ var confirmLower;
 var confirmUpper;
 var confirmNumber;
 var confirmSpecial;
-var userChoices;
+var userChoices = [];
 
 var lowerCase = ["a", "b", "c","d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n","o","p","q","r", "s","t", "u", "v", "w", "x", "y", "z",];
 
 var BlankUpper = [];
 var toUpper = function (x) {
-    return x.toUppercase();
+    return x.toUpperCase();
 };
-upperCase = lowerCase.map(toUpper);
+var upperCase = lowerCase.map(toUpper);
 
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9,];
 var special = ["!", "@", "#", "$", "%", "^","&", "*", "(", ")", "_", "+", "<", ">", "?", ";", "'", "/",];
 
+// main function
 function writePassword() {
+
+    console.log('This is writePassword!')
     var password = generatePassword();
-    var passwordText = document.querySelector("password");
+
+    var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
 
@@ -28,14 +32,82 @@ function writePassword() {
 }
 
 function generatePassword() {
-    passwordLength = prompt("How many characters would you like your password to be? Choose any amount between 8 and 128 characters.");
-    console.log("Password length" + passwordLength);
+    var newPassword = "";
 
-    if (passwordLength) {
-        alert("Required value");
+    do {
+        var isInputValid = true;
+
+        passwordLength = prompt("How many characters would you like your password to be? Choose any amount between 8 and 128 characters.");
+        // convert the answer to a number
+        passwordLength = parseInt(passwordLength)
+
+        console.log("Password length" + passwordLength);
+    
+        if (!passwordLength) {
+            isInputValid = false;
+            alert("Required value");
+        } else if (passwordLength < 8 || passwordLength > 128) {
+            isInputValid = false;
+            alert("You have to choose between 8 & 128 characters, try again.");
+            console.log("Password Length" + passwordLength);
+        }
+    } while (!isInputValid)
+
+    // var confirmLower;
+    // var confirmUpper;
+    // var confirmNumber;
+    // var confirmSpecial;
+
+    confirmLower = confirm("Do you want to include lowercase?")
+    console.log(confirmLower)
+
+    if(confirmLower) {
+        // add lowercase letters!
+        // [] + [a, b, c, d, e....] = [a, b, c, d, e....]
+        userChoices = userChoices.concat(lowerCase);
     }
 
-} else if (passwordLength < 8 || passwordLength > 128) {
-    passwordLength = prompt ("You have to choose between 8 & 128 characters, try again.");
-    console.log("Password Length" + passwordLength);
+    confirmUpper = confirm("Do you want to include uppercase?")
+    console.log(confirmUpper)
+
+    if(confirmUpper) {
+        // add uppercase letters!
+        // [a, b, c, d, e....] + [A, B, C, D, E] = [a, b, c..... , A, B, C ...]
+        userChoices = userChoices.concat(upperCase);
+    }
+
+
+    confirmSpecial = confirm("Do you want to include special characters?")
+    console.log(confirmSpecial)
+
+    if(confirmSpecial) {
+        // add special characters!
+        // [] + ["!", "@", "#", "$", "%", "^","&", "*",...] = [a, b, c, d, e....]
+        userChoices = userChoices.concat(special);
+    }
+
+    console.log(userChoices)
+
+    // after confirming the userChoices
+    // we now get random characters from the userChoices
+    // using passwordLength as length
+
+    for (let index = 0; index < passwordLength; index++) {
+
+        // generate a random number between userChoices length
+        var randomIndex = Math.floor(Math.random() * userChoices.length);
+
+        var randomCharacter = userChoices[randomIndex]
+        newPassword = newPassword + randomCharacter
+    }
+
+    console.log(newPassword)
+
+    return newPassword;
+
+
+    
 }
+
+
+generateBtn.addEventListener("click", writePassword)
