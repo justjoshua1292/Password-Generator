3c2dbc75421ce6ad339ca028ded31e1e7ecebc1b
Joshua Harris <Justjoshua1292@gmail.com>
2021-06-21 11:05:25 -0400

    working on var statements

---
+++ script.js
@@ -0,0 +1,12 @@
+var generateBtn = document.querySelector("#generate");
+
+var passwordLength;
+var confirmLower;
+var confirmUpper;
+var confirmNumber;
+var confirmSpecial;
+var userChoices;
+
+
+
+var BlankUpper = [];
